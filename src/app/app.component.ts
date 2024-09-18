22d080d3dbca9312f59473ab711b8ad93d9a3e67
ChrisferDev <ferchris82@hotmail.com>
2024-09-18 12:49:02 -0500

    Versión 0.1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CounterComponent } from './counter/counter.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CounterComponent, CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'app-angular';
+  title: string = 'Hola mundo angular desde componente!';
+
+  users: string[] = ['Pepe', 'Maria', 'Juan', 'Andres'];
+
+  visible: boolean = false;
+
+  setVisible(): void {
+    this.visible = this.visible ? false : true;
+    console.log('hemos hecho clic en setVisible');
+  }
 }
+
---
+++ src/app/counter/counter.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-counter',
+  standalone: true,
+  imports: [],
+  templateUrl: './counter.component.html'
+})
+export class CounterComponent {
+
+  counter: number = 0;
+  
+  setCounter(): void {
+    this.counter = this.counter + 1;
+  }
+}
